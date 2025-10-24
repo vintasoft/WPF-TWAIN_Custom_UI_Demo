79a24cb01c12471a0774d3c4d59b220bfb8df454
vintasoft <alex@vintasoft.com>
2025-10-24 14:50:44 +0300

    Source codes update

--- CSharp/MainWindow.xaml.cs
+++ CSharp/MainWindow.xaml.cs
@@ -82,12 +82,9 @@ namespace WpfTwainCustomUIDemo
 
         public MainWindow()
         {
-            // register the evaluation license for VintaSoft TWAIN .NET SDK
-            Vintasoft.Twain.TwainGlobalSettings.Register("REG_USER", "REG_EMAIL", "EXPIRATION_DATE", "REG_CODE");
-
             InitializeComponent();
 
-            this.Title = string.Format("VintaSoft WPF TWAIN Custom UI Demo v{0}", TwainGlobalSettings.ProductVersion);
+            this.Title = string.Format("VintaSoft WPF TWAIN Custom UI Demo v{0}", TwainEnvironment.ProductVersion);
 
             _deviceManager = new DeviceManager(this);
         }
